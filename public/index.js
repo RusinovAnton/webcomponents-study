5c6ca4f733312e06df691564599a003759328b62
Rusinov Anton <rusinovantondev@gmail.com>
2017-12-29 17:51:29 +0200

    Add webcomponent

--- public/index.js
+++ public/index.js
@@ -1,1 +1,35 @@
-console.log('Hi');
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    @keyframes roll {
+        to {
+          transform: rotate(360deg);
+        }
+    }
+    
+    div {
+      width: 50px;
+      height: 50px;
+      margin: 50px;
+      background-color: #ff93f0;
+      animation: roll 3s linear infinite;
+    }
+  </style>
+  
+  <div></div>
+`;
+
+class RollingCube extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content);
+  }
+
+  attributeChangedCallback() {
+    console.log('attribute changed');
+  }
+}
+
+customElements.define('rolling-cube', RollingCube);
